5b24d0d4fbb004614a59297d81a3686ba0aa9db4
raviteja kurva <raviteja.kurva@profiflitzer.de>
2023-03-22 19:32:39 +0100

    second commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pts',
-    version='0.5.X',
+    version='0.1',
     description="PyTorch Probabilistic Time Series Modeling framework",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
