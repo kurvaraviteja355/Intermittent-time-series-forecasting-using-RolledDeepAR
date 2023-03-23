2041b053268974f4d74bc9fddc5cebde4655a1b9
raviteja kurva <raviteja.kurva@profiflitzer.de>
2023-03-23 11:21:19 +0100

    first commit

--- setup.py
+++ setup.py
@@ -9,10 +9,18 @@ setup(
 
     url='https://github.com/kurvaraviteja355/Intermittent-time-series-forecasting-using-RolledDeepAR',
     license='MIT',
-    packages=['pts'],
+    packages=find_packages(include=["pts"]),
     include_package_data=True,
     zip_safe=True,
     python_requires=">=3.6",
+    install_requires=[
+        "torch>=1.8.0",
+        'pydantic==1.4.0',
+        'python-rapidjson',
+        'pytorch-warmup',
+        'tqdm',
+        "gluonts",],
+
     test_suite='tests',
     tests_require = [
         'flake8',
